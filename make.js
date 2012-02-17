cb4fbbe63d05e9ab50ddb32da2b9b1dce5eceea2
Kelley van Evert <kelleyvanevert@gmail.com>
2012-02-17 18:42:54 +0100

    COMPLETE

--- make.js
+++ make.js
@@ -1,8 +1,10 @@
 
-var _ = require("./underscore"),
+var _ = require("./public/underscore"),
     fs = require("fs"),
-    template,
-    pages = [];
+    template = null,
+    pages = [],
+    args = Array.prototype.slice.call(process.argv, 2),
+    outputfolder = "public";
 
 _.templateSettings = {
   evaluate    : /\[%([\s\S]+?)%\]/g,
@@ -11,11 +13,20 @@ _.templateSettings = {
 };
 
 console.log("Hi!");
+if (args.length < 2) {
+  console.log("  !! ERROR wrong argument count. You must supply:");
+  console.log("     (1) template file");
+  console.log("     (2) output directory");
+  process.exit();
+}
+outputfolder = args[1];
+console.log("output directory: " + outputfolder);
+
 console.log("reading template...");
 try {
-  template = _.template(fs.readFileSync("template.html", "utf8"));
+  template = _.template(fs.readFileSync(args[0], "utf8"));
 } catch (e) {
-  console.log("  !! ERROR reading template.html");
+  console.log("  !! ERROR reading/parsing template file " + args[0]);
   process.exit();
 }
 console.log("  DONE");
@@ -69,7 +80,10 @@ pages.map(function (page) {
         pages: pages
       },
       html = template(context);
-    fs.writeFileSync(page.url[currentlanguage], html, "utf8");
+    fs.writeFileSync(outputfolder + "/" + page.url[currentlanguage], html, "utf8");
   });
 });
 console.log("  DONE");
+
+console.log("");
+console.log("Bye!");
